dd0205e44f89466d7076ba2b0272d85cfedc0122
Chris Ovrebo <chris.ovrebo@gmail.com>
2020-07-25 13:39:41 -0500

    regex and bite 2

---
+++ 10_regex/regex.py
@@ -0,0 +1,52 @@
+# bite 2
+import re
+
+COURSE = ('Introduction 1 Lecture 01:47'
+          'The Basics 4 Lectures 32:03'
+          'Getting Technical!  4 Lectures 41:51'
+          'Challenge 2 Lectures 27:48'
+          'Afterword 1 Lecture 05:02')
+TWEET = ('New PyBites article: Module of the Week - Requests-cache '
+         'for Repeated API Calls - http://pybit.es/requests-cache.html '
+         '#python #APIs')
+HTML = ('<p>pybites != greedy</p>'
+        '<p>not the same can be said REgarding ...</p>')
+TWEET2 = ('PyBites My Reading List | 12 Rules for Life - #books '
+                 'that expand the mind! '
+                 'http://pbreadinglist.herokuapp.com/books/'
+                 'TvEqDAAAQBAJ#.XVOriU5z2tA.twitter'
+                 ' #psychology #philosophy')
+
+
+def extract_course_times(course=COURSE):
+    """Return the course timings from the passed in
+       course string. Timings are in mm:ss (minutes:seconds)
+       format, so taking COURSE above you would extract:
+       ['01:47', '32:03', '41:51', '27:48', '05:02']
+       Return this list.
+    """
+    matches = re.findall(r'\d{2}:\d{2}', course)
+    return matches
+
+
+def get_all_hashtags_and_links(tweet=TWEET2):
+    """Get all hashtags and links from the tweet text
+       that is passed into this function. So for TWEET
+       above you need to extract the following list:
+       ['http://pybit.es/requests-cache.html',
+        '#python',
+        '#APIs']
+       Return this list.
+    """
+    matches = re.findall(r'http://.*?(?=\s)|#\w+', tweet)
+    return matches
+
+
+def match_first_paragraph(html=HTML):
+    """Extract the first paragraph of the passed in
+       html, so for HTML above this would be:
+       'pybites != greedy' (= content of first paragraph).
+       Return this string.
+    """
+    matches = re.findall(r'(?<=<p>)(.*?)(?=</p>)', html)
+    return matches[0]
