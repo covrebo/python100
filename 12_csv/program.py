5b4bae742714dabdc85afc1648d2d372922a6b8d
Chris Ovrebo <chris.ovrebo@gmail.com>
2020-08-02 07:59:14 -0500

    csv challenge day 1 - weather data

---
+++ 12_csv/program.py
@@ -0,0 +1,32 @@
+import research
+
+
+def main():
+    print(f'Weather data for Seattle, 2014-2015')
+    print()
+
+    # Initialize the data
+    research.init()
+
+    print('The hottest 5 days:')
+    days = research.hot_days()
+    for idx, d in enumerate(days[:5]):
+        print(f'#{idx + 1}, {d.actual_max_temp} F on {d.date}')
+
+    print()
+
+    print('The coldest 5 days:')
+    days = research.cold_days()
+    for idx, d in enumerate(days[:5]):
+        print(f'#{idx + 1}, {d.actual_min_temp} F on {d.date}')
+
+    print()
+
+    print('The wettest 5 days:')
+    days = research.wet_days()
+    for idx, d in enumerate(days[:5]):
+        print(f'#{idx + 1}, {d.actual_precipitation} inches of rain on {d.date}')
+
+
+if __name__ == '__main__':
+    main()
